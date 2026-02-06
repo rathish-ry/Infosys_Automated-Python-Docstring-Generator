01a356b480d7d861690a72037a5b3a2bca2fbfd9
rathish-ry <727723euai097@skcet.ac.in>
2026-02-06 15:18:45 +0530

    add docstrings to calculator

--- project_infosys/low_coverage.py
+++ project_infosys/low_coverage.py
@@ -1,12 +1,44 @@
 class Calculator:
+    """Manages calculator functionality."""
     def add(self, a, b):
+        """
+        Perform operation with a and b.
+        
+        Args:
+            self: The class or instance.
+            a: The a.
+            b: The b.
+        """
         return a + b
     
     def subtract(self, a, b):
+        """
+        Perform operation with a and b.
+        
+        Args:
+            self: The class or instance.
+            a: The a.
+            b: The b.
+        """
         return a - b
     
     def multiply(self, a, b):
+        """
+        Perform operation with a and b.
+        
+        Args:
+            self: The class or instance.
+            a: The a.
+            b: The b.
+        """
         return a * b
 
 def divide(x, y):
+    """
+    Perform operation with x and y.
+    
+    Args:
+        x: The x.
+        y: The y.
+    """
     return x / y
